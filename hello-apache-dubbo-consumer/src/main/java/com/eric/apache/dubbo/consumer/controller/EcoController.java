76fc5d05c804e5553a41ee991bc71a50133cacb4
EricShen <ahsbt@126.com>
2019-11-28 18:04:21 +0800

    add nacos config

--- hello-apache-dubbo-consumer/src/main/java/com/eric/apache/dubbo/consumer/controller/EcoController.java
+++ hello-apache-dubbo-consumer/src/main/java/com/eric/apache/dubbo/consumer/controller/EcoController.java
@@ -2,6 +2,8 @@ package com.eric.apache.dubbo.consumer.controller;
 
 import com.eric.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019-11-28
  */
 @RestController
+@RefreshScope
 public class EcoController {
 
   @Reference(version = "1.0.0")
   private EchoService echoService;
+
+  @Value("${user.name}")
+  private String username;
 
   @GetMapping(value = "echo/{str}")
   public String echo(@PathVariable String str) {
@@ -22,8 +28,13 @@ public class EcoController {
   }
 
   @GetMapping(value = "lb")
-  public String lb(){
+  public String lb() {
     return echoService.lb();
+  }
+
+  @GetMapping(value = "config")
+  public String nacosConfig() {
+    return "Get config of user.name= " + username + " from nacos";
   }
 
 }
